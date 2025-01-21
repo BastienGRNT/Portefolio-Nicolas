5d42a1e4a3c52bd4945abc1c07fb782a88f1d903
greno <bastien.grenot@gmail.com>
2025-01-21 16:46:59 +0100

    HTML Projet & Logo

--- Script/scriptDev.js
+++ Script/scriptDev.js
@@ -1,28 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
     const projects = [
         {
-            title: "Projet Pendu",
+            title: "Projet Pendu C",
             image: "../imageDev/image1.jpg",
-            link: "projet1.html",
-            github: "https://github.com/BastienGRNT/pendu-c"
+            link: "ProjectPage/projet1.html",
+            github: "https://github.com/BastienGRNT/pendu-c",
+            languageLogo: "../iconeDev/c.png"
+        },
+        {
+            title: "Projet Pendu JS",
+            image: "../imageDev/image2.png",
+            link: "ProjectPage/projet1.html",
+            github: "https://github.com/BastienGRNT/pendu-js",
+            languageLogo: "../iconeDev/js.svg"
         },
         {
             title: "Projet Siteweb",
-            image: "../imageDev/image2.png",
-            link: "projet2.html",
-            github: "https://github.com/BastienGRNT/MoviesFinder"
+            image: "../imageDev/image3.png",
+            link: "ProjectPage/projet1.html",
+            github: "https://github.com/BastienGRNT/MoviesFinder",
+            languageLogo: "../iconeDev/postgre.svg"
+        },
+        {
+            title: "Projet Logiciel",
+            image: "../imageDev/image4.png",
+            link: "ProjectPage/projet1.html",
+            github: "https://github.com/BLID-id/hotelapp",
+            languageLogo: "../iconeDev/csharp.svg"
         },
         {
             title: "Projet Application",
-            image: "../imageDev/image1.jpg",
-            link: "projet3.html",
-            github: "https://github.com/user/projet-application"
-        },
-        {
-            title: "Projet API",
-            image: "../imageDev/image1.jpg",
-            link: "projet4.html",
-            github: "https://github.com/user/projet-api"
+            image: "../imageDev/image5.png",
+            link: "ProjectPage/projet1.html",
+            github: "https://github.com/BastienGRNT/APP_LB",
+            languageLogo: "../iconeDev/flutter.svg"
         },
     ];
 
@@ -32,29 +43,35 @@ document.addEventListener("DOMContentLoaded", function() {
         const projectDiv = document.createElement("div");
         projectDiv.classList.add("project-card");
 
-        // Titre
+        // Titre avec logo
         const titleDiv = document.createElement("div");
         titleDiv.classList.add("project-title");
-        titleDiv.textContent = project.title;
 
-        // Image container avec un texte de remplacement
+        const titleText = document.createElement("span");
+        titleText.textContent = project.title;
+
+        const langLogo = document.createElement("img");
+        langLogo.src = project.languageLogo;
+        langLogo.alt = `${project.title} logo`;
+        langLogo.classList.add("language-logo");
+
+        titleDiv.appendChild(titleText);
+        titleDiv.appendChild(langLogo);
+
+        // Image container
         const imageDiv = document.createElement("div");
         imageDiv.classList.add("project-image");
 
-        if (project.image) {
-            const imgElement = document.createElement("img");
-            imgElement.src = project.image;
-            imgElement.alt = `Screenshot de ${project.title}`;
-            imageDiv.appendChild(imgElement);
-        } else {
-            imageDiv.textContent = "Screen shot";  // Texte si l'image ne se charge pas
-        }
+        const imgElement = document.createElement("img");
+        imgElement.src = project.image;
+        imgElement.alt = `Screenshot de ${project.title}`;
+        imageDiv.appendChild(imgElement);
 
         // Lien GitHub
         const githubLink = document.createElement("a");
         githubLink.classList.add("github-link");
         githubLink.href = project.github;
-        githubLink.target = "_blank";  // Ouvrir le lien dans un nouvel onglet
+        githubLink.target = "_blank";
         githubLink.textContent = "Voir sur GitHub";
 
         // Ajout des éléments à la carte du projet
@@ -62,7 +79,7 @@ document.addEventListener("DOMContentLoaded", function() {
         projectDiv.appendChild(imageDiv);
         projectDiv.appendChild(githubLink);
 
-        // Redirection lors du clic (sur l'image ou le titre)
+        // Redirection lors du clic sur la carte entière
         projectDiv.addEventListener("click", function() {
             window.location.href = project.link;
         });
